ca0a932a79fa4798c235292f930f0fa69fff0e7f
Emmanuel Ariza <emmanuel@emmanuel.emmanuel>
2017-09-25 21:10:43 -0500

    Now TweetAdmin is read only

--- tweetsDB/admin.py
+++ tweetsDB/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 from tweetsDB import models
+from read_only_admin.admin import ReadonlyAdmin
 
-# Register your models here.
+
+class ReadOnlyAdminBase(ReadonlyAdmin):
+
+    def get_readonly_fields(self, request, obj=None):
+        print [f.name for f in self.model._meta.fields if f.name not in ["id"]]
+        return self.fields or [f.name for f in self.model._meta.fields if f.name not in ["id"]]
+    
+    def has_add_permission(self, request):
+        return False
+         
+    def has_delete_permission(self, request, obj=None):
+        return False       
+
+
+
 @admin.register(models.Tweet)
-class TweetAdmin(admin.ModelAdmin):
+class TweetAdmin(ReadOnlyAdminBase):
 	list_display  = ('text', 'likes', 'retweets', 'responses', 'timestamp', 'url')
 	list_filter = ('likes', 'retweets', 'responses')
 
-	
+	def get_readonly_fields(self, request, obj=None):
+		read_only = ReadOnlyAdminBase.get_readonly_fields(self, request, obj=obj) + ['url']
+		read_only.remove('source') 
+		return read_only
 
--- twitter_dashboard/settings.py
+++ twitter_dashboard/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tweetsDB'
+    'tweetsDB',
+    'read_only_admin'
 ]
 
 MIDDLEWARE = [
