2684154faf8f92e2059c06a35157c6579af58e84
Emmanuel Ariza <emmanuel@emmanuel.emmanuel>
2017-09-25 20:29:23 -0500

    Created model, admin and command for populate from file

--- tweetsDB/admin.py
+++ tweetsDB/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from tweetsDB import models
 
 # Register your models here.
+@admin.register(models.Tweet)
+class TweetAdmin(admin.ModelAdmin):
+	list_display  = ('text', 'likes', 'retweets', 'responses', 'timestamp', 'url')
+	list_filter = ('likes', 'retweets', 'responses')
 
-admin.site.register(models.tweet)
+	
+
--- tweetsDB/models.py
+++ tweetsDB/models.py
@@ -4,14 +4,20 @@ from django.db import models
 
 # Create your models here.
 
-class tweet(models.Model):
-	tweet_id = models.CharField(max_length = 255)
+class Tweet(models.Model):
+	tweet_id = models.CharField(max_length = 255, unique =  True)
 	in_reply_to_status_id = models.CharField(max_length = 255)
 	in_reply_to_user_id = models.CharField(max_length = 255)
 	timestamp = models.DateTimeField()
 	source = models.CharField(max_length = 255)
 	text = models.CharField(max_length = 255)
 	expanded_urls = models.CharField(max_length = 255)
-	likes = models.IntegerField()
-	retweets = models.IntegerField() 
-	responses = models.IntegerField()
+	likes = models.IntegerField(null=True)
+	retweets = models.IntegerField(null=True) 
+	responses = models.IntegerField(null=True)
+
+	def url(self):
+		return '<a target="_blank" href="https://twitter.com/statuses/%s">link to tweet</a>' % (self.tweet_id)
+
+	url.short_description = 'Url'
+	url.allow_tags = True
